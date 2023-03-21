82f0c212e023420624784df1430297fdecd5c207
Krzysztof Dziedzic <k.dziedzic@moodup.team>
2023-03-21 12:56:36 +0100

    Fix app issues

--- packages/google_maps_flutter/google_maps_flutter/example/lib/readme_sample.dart
+++ packages/google_maps_flutter/google_maps_flutter/example/lib/readme_sample.dart
@@ -12,7 +12,7 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +25,14 @@ class MyApp extends StatelessWidget {
 
 // #docregion MapSample
 class MapSample extends StatefulWidget {
-  const MapSample({Key? key}) : super(key: key);
+  const MapSample({super.key});
 
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
 class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
